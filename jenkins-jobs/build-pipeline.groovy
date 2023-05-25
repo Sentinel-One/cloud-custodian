79a84f3673648302f598bdcd3da605938dd3fa56
vikash.kumarsinha <vikash.kumarsinha@sentinelone.com>
2023-05-25 17:24:00 +0530

    [CLS-12878] feature: update the Python package's release version to 0.9.26.3

--- jenkins-jobs/build-pipeline.groovy
+++ jenkins-jobs/build-pipeline.groovy
@@ -10,7 +10,7 @@ import groovy.transform.Field
 @Field isRelease = false
 @Field isPRBuild = false
 
-@Field artVersion = "0.9.26.1"
+@Field artVersion = "0.9.26.3"
 
 timeout(activity: true, time: 10) {
     timestamps {
