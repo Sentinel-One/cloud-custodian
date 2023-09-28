329dab99499f9b429e746caeb62ab0c00cf73328
vikashkumarsinhasentinelone <vikash.kumarsinha@sentinelone.com>
2023-09-28 16:51:06 +0530

    [CLS-24161] feature: Add new filters to support AWS CIS Benchmark detection rules for AWS Account

--- jenkins-jobs/release-pipeline.groovy
+++ jenkins-jobs/release-pipeline.groovy
@@ -10,7 +10,7 @@ import groovy.transform.Field
 @Field isRelease = false
 @Field isPRBuild = false
 
-@Field artVersion = "0.9.26.4.14"
+@Field artVersion = "0.9.26.4.15"
 
 timeout(activity: true, time: 10) {
     timestamps {
