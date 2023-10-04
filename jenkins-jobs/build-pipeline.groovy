3f79014e76b8a1c38e706d4954955b9c5d88e58f
vikashkumarsinhasentinelone <vikash.kumarsinha@sentinelone.com>
2023-10-04 13:43:25 +0530

    [CLS-24526] feature: Upgrade s1-c7n version

--- jenkins-jobs/build-pipeline.groovy
+++ jenkins-jobs/build-pipeline.groovy
@@ -10,7 +10,7 @@ import groovy.transform.Field
 @Field isRelease = false
 @Field isPRBuild = false
 
-@Field artVersion = "0.9.26.4.18"
+@Field artVersion = "0.9.26.4.20"
 
 
 timeout(activity: true, time: 10) {
